acf31fe4dfd6cebcb89563023ac234117d26b7bf
bamitha ekanayake <bamithabekanayake44@gmail.com>
2025-09-03 01:57:25 +0530

    new

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import { PRODUCTS } from "../../products";
 
 export const Product = (props) => {
-  const { id, productName, price, productImage } = props.data;
+  const { productName, price, productImage } = props.data;
 
-  return <div>{productName}</div>;
+  return (
+    <div className="product">
+      <img src={productImage} alt={productName} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price.toFixed(2)}</p>
+      </div>
+    </div>
+  );
 };
