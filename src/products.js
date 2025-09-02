c1c463e3534a6ca5da39e8043cb9219f20e5ae91
bamitha ekanayake <bamithabekanayake44@gmail.com>
2025-09-03 00:45:43 +0530

    initial commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import image1 from "./assets/1.jpg";
+import image2 from "./assets/2.jpg";
+import image3 from "./assets/3.jpg";
+import image4 from "./assets/4.jpg";
+import image5 from "./assets/5.jpg";
+import image6 from "./assets/6.jpg";
+import image7 from "./assets/7.jpg";
+import image8 from "./assets/8.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "product-1",
+    price: 59.0,
+    productImage: image1,
+  },
+  {
+    id: 2,
+    productName: "product-2",
+    price: 89.0,
+    productImage: image2,
+  },
+  {
+    id: 3,
+    productName: "product-3",
+    price: 29.0,
+    productImage: image3,
+  },
+  {
+    id: 4,
+    productName: "product-4",
+    price: 29.0,
+    productImage: image4,
+  },
+  {
+    id: 5,
+    productName: "product-5",
+    price: 19.0,
+    productImage: image5,
+  },
+  {
+    id: 6,
+    productName: "product-6",
+    price: 49.0,
+    productImage: image6,
+  },
+  {
+    id: 7,
+    productName: "product-7",
+    price: 39.0,
+    productImage: image7,
+  },
+  {
+    id: 8,
+    productName: "product-8",
+    price: 99.0,
+    productImage: image8,
+  },
+];
