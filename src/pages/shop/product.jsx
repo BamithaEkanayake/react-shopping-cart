c92a0dd892b6ebbd19367f29cd94e4c405495686
Bamitha Ekanayake <136635825+BamithaEkanayake@users.noreply.github.com>
2025-09-03 01:32:01 +0530

    Revert "shop page updated and added shop.css"

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -2,17 +2,7 @@ import React from "react";
 import { PRODUCTS } from "../../products";
 
 export const Product = (props) => {
-  const { productName, price, productImage } = props.data;
+  const { id, productName, price, productImage } = props.data;
 
-  return (
-    <div className="product">
-      <img src={productImage} alt={productName} />
-      <div className="description">
-        <p>
-          <b>{productName}</b>
-        </p>
-        <p>${price.toFixed(2)}</p>
-      </div>
-    </div>
-  );
+  return <div>{productName}</div>;
 };
